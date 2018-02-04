bdcdc1376004d6890f8624f667e1940633f76133
bmblby <castiglia.giuliano@googlemail.com>
2018-02-04 19:26:55 +0100

    added mongoclient for request of topic distribution

--- server.js
+++ server.js
@@ -10,6 +10,29 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 
+// mongodb
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+function findArguments(db, collection, argumentList, callback) {
+  console.log(argumentList);
+  var collection = db.collection(collection);
+  collection.find({
+    id: { $in: argumentList}
+  }).toArray(function (err, res) {
+    assert.equal(err, null);
+    data = [];
+    for(arg of res) {
+      let argument = {};
+      argument['id'] = arg['id'];
+      argument['topics'] = arg['topics'];
+      data.push(argument);
+    }
+    console.log('Found ' + res.length + ' documents within database' );
+    callback(data);
+  })
+}
+
 var options = {
   root: './',
 };
@@ -17,8 +40,20 @@ var options = {
 app.use(bodyParser.json());
 app.post('/', function(req, res) {
   let argumentList = req.body;
-  console.log(argumentList);
-  res.json(req.body);
+  console.log('number of arguments received: ' + argumentList.length);
+
+  let url = 'mongodb://localhost:27017/wikimodel';
+  MongoClient.connect(url, function (err, client) {
+    if(err) throw err;
+    console.log('connected successfully to database!');
+
+    var db = client.db('wikimodel');
+    findArguments(db, 'wiki_arg', argumentList, function (result) {
+      // console.log(result);
+      res.json(result);
+      client.close();
+    })
+  })
 })
 
 app.get('/', (req, res) => {
